bd5d00d45dd16b49f51a43ca300ac16a9675b068
DerekPeacock <derek.peacock@yahoo.com>
2019-09-15 14:15:59 +0100

    Customised the layout and added an about and a  Hello page

---
+++ CoreRazorContoso/Pages/Hello.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreRazorContoso.Pages
+{
+    public class HelloModel : PageModel
+    {
+        public String name;
+
+        public void OnGet()
+        {
+            name = null;
+        }
+
+        public void OnPost()
+        {
+            this.name = Request.Form["name"];
+        }
+    }
+}
